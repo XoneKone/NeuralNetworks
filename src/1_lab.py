4c7d2c9f5427ca257af2eb15640334415d1e408d
XoneKone <mr.blackmarch@mail.ru>
2023-09-17 20:47:42 +0300

    lab1 - start

---
+++ src/1_lab.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 4
+def act(x):
+    pass
+
+
+input_vectors = np.array([[0.97, 0.20],
+                          [1.0, 0],
+                          [-0.72, 0.7],
+                          [-0.67, 0.74],
+                          [-0.8, 0.6],
+                          [0, -1.0],
+                          [0.2, -0.97],
+                          [-0.3, -0.95]
+                          ])
+
+
+
