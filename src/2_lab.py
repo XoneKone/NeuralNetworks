7157f5116b340dbb4a2781102d7e48eff894e1de
XoneKone <mr.blackmarch@mail.ru>
2023-10-22 23:30:11 +0300

    lab2 - started second task

--- src/2_lab.py
+++ src/2_lab.py
@@ -32,5 +32,100 @@ def task_1():
     print(f'(1, 1) = {go(C2[1])}')
 
 
+def task_2():
+    import random
+    random.seed(20)
+
+    def generate_marked_dots(n=20):
+        dots = [[round(random.random(), 5), round(random.random(), 5)] for i in range(n)]
+        for dot in dots:
+            if dot[0] < dot[1]:
+                dot.append(0)
+            if dot[0] > dot[1]:
+                dot.append(1)
+        return dots
+
+    def act_adaline(x):
+        if x < 0:
+            return -1
+        if x > 0:
+            return 1
+    def df_relu(x):
+        return 0 if x < 0 else 1
+
+    def act_perceptron(x):
+
+    def df_sig(x):
+        return 0.5 * (1 + x) * (1 - x)
+
+    def go_forward_adaline(input_vectors, W):
+        sum = np.dot(W, input_vectors)
+        y = relu(sum)
+        return y
+
+    def go_forward_perceptron(input_vectors, W):
+        sum = np.dot(W, input_vectors)
+        y = relu(sum)
+        return y
+
+    def train(epoch, W):
+        lmd = 0.01
+        N = 1000
+        count = len(epoch)
+        for k in range(N):
+            x = epoch[np.random.randint(0, count)]
+            y = go_forward(x[:2], W)
+            e = y - x[-1]
+            delta = e * df_relu(y)
+            W = W - lmd * delta * np.array(x[:2])
+
+    W = np.array([round(np.random.uniform(-1, 1), 2), round(np.random.uniform(-1, 1), 2)])
+    train_marked_dots = generate_marked_dots()
+    train(train_marked_dots, W)
+
+    for x in train_marked_dots:
+        y = go_forward(x[:2], W)
+        print(f"Выходное значение НС: {y} => {x[-1]}")
+
+
+def task_3():
+    def relu(x):
+        return np.maximum(x, 0.0)
+
+    def df_relu(x):
+        return 0 if x < 0 else 1
+
+    def sig(x):
+        return 2 / (1 + np.exp(-x)) - 1
+
+    def df_sig(x):
+        return 0.5 * (1 + x) * (1 - x)
+
+    def go_forward(input_vectors, W):
+        sum = np.dot(W, input_vectors)
+        y = relu(sum)
+        return y
+
+    def train(epoch, W):
+        lmd = 0.01
+        N = 1000
+        count = len(epoch)
+        for k in range(N):
+            x = epoch[np.random.randint(0, count)]
+            y = go_forward(x[:2], W)
+            e = y - x[-1]
+            delta = e * df_relu(y)
+            W = W - lmd * delta * np.array(x[:2])
+
+    W = np.array([round(np.random.uniform(-1, 1), 2), round(np.random.uniform(-1, 1), 2)])
+    train_marked_dots = generate_marked_dots()
+    train(train_marked_dots, W)
+
+    for x in train_marked_dots:
+        y = go_forward(x[:2], W)
+        print(f"Выходное значение НС: {y} => {x[-1]}")
+
+
 if __name__ == '__main__':
-    task_1()
+    # task_1()
+    task_2()
